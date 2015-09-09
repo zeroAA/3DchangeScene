e2345a3083e5a514c787f193f8cf3ed7b2f5f024
zero <zero-87@163.com>
2015-09-09 15:29:34 +0800

    all

--- src/data.java
+++ src/data.java
@@ -47,7 +47,7 @@ public class data {
 	}
 
 	public void log(String str) {
-		// System.out.println(str);
+		 System.out.println(str);
 	}
 
 	public static void main(String[] args) {
@@ -59,7 +59,9 @@ public class data {
 
 			data read = new data();
 
-			String path = "/Users/zhusu/Desktop/data/";
+			String path = "/Users/zhusu/Desktop/3DScene/json/";
+			
+			String outpath = "/Users/zhusu/Desktop/3DScene/out/";
 
 			File dir = new File(path);
 
@@ -71,134 +73,299 @@ public class data {
 					String JsonContext = read.ReadFile(path + dirs[k]);
 
 					JSONObject jsonObject = JSONObject.fromObject(JsonContext);
-
-					JSONObject Content = jsonObject.getJSONObject("Content")
-							.getJSONObject("Content");
-
+					
+					
+					JSONObject Content =  jsonObject.getJSONObject("Content").getJSONObject("Content");
+					
 					JSONObject ObjectData = Content.getJSONObject("ObjectData");
-
-					JSONArray Children = ObjectData.getJSONArray("Children");
-
-					os = new FileOutputStream(new File(path
+					
+					JSONArray Children  =  ObjectData.getJSONArray("Children");
+					
+					os = new FileOutputStream(new File(outpath
 							+ dirs[k].replaceFirst(".json", ".data")));
 
 					bout = new ByteArrayOutputStream();
 					dout = new DataOutputStream(bout);
 
 					int num = 0;
-
-					for (int i = 0; i < Children.size(); ++i) {
-						JSONObject jdata = Children.getJSONObject(i);
-						if (jdata.getString("ctype").equals(
-								"ArmatureNodeObjectData")) {
-							num++;
-						}
-					}
-
-					read.log("size: " + num);
+					
+					num = Children.size();
+					
+					read.log("num: " + num);
+					
 					dout.writeInt(num);
 
-					int endY = 0;
-
+					int writeNum = 0;
+					
 					for (int i = 0; i < Children.size(); ++i) {
 
 						JSONObject jdata = Children.getJSONObject(i);
-
-						if (jdata.getString("ctype").equals(
-								"ArmatureNodeObjectData")) {
-
-							String name = jdata.getJSONObject("FileData")
-									.getString("Path");
-
-							name = name.replaceFirst(".ExportJson", ".csb");
-
-							read.log("Path: " + name);
-
+						
+						if(jdata.getString("ctype").equals("UserCameraObjectData")){
+							
+							writeNum++;
+							
+							int type = 1;
+							read.log("type: " + type);
+							
+							dout.writeInt(type);
+							
+							String name = jdata.getString("Name");
+							read.log("name: "+name);
 							dout.writeUTF(name);
-
-							read.log("IsLoop: " + jdata.getBoolean("IsLoop"));
-
-							dout.writeBoolean(jdata.getBoolean("IsLoop"));
-
-							read.log("CurrentAnimationName: "
-									+ jdata.getString("CurrentAnimationName"));
-
-							dout.writeUTF(jdata
-									.getString("CurrentAnimationName"));
-
-							read.log("X: "
-									+ jdata.getJSONObject("Position").getInt(
-											"X"));
-
-							dout.writeInt(jdata.getJSONObject("Position")
-									.getInt("X"));
-
-							read.log("Y: "
-									+ jdata.getJSONObject("Position").getInt(
-											"Y"));
-
-							dout.writeInt(jdata.getJSONObject("Position")
-									.getInt("Y"));
-
-							read.log("ScaleX: "
-									+ jdata.getJSONObject("Scale").getDouble(
-											"ScaleX"));
-
-							dout.writeFloat((float) jdata
-									.getJSONObject("Scale").getDouble("ScaleX"));
-
-							read.log("ScaleY: "
-									+ jdata.getJSONObject("Scale").getDouble(
-											"ScaleY"));
-
-							dout.writeFloat((float) jdata
-									.getJSONObject("Scale").getDouble("ScaleY"));
-
-							float rotation = 0;
-							if (jdata.has("Rotation")) {
-								rotation = (float) jdata.getDouble("Rotation");
-							}
-
-							read.log("Rotation: " + rotation);
-							dout.writeFloat(rotation);
-
-							read.log("IconVisible: "
-									+ jdata.getBoolean("IconVisible"));
-
-							dout.writeBoolean(jdata.getBoolean("IconVisible"));
-
-							String userData = "";
-
-							if (jdata.has("UserData")) {
-								userData = jdata.getString("UserData");
-							}
-
-							read.log("UserData: " + userData);
-
-							dout.writeUTF(userData);
-
-							read.log("Tag: " + jdata.getInt("Tag"));
-
-							dout.writeInt(jdata.getInt("Tag"));
-
-							read.log("Name: " + jdata.getString("Name"));
-
-							dout.writeUTF(jdata.getString("Name"));
-
-						} else if (jdata.getString("ctype").equals(
-								"SingleNodeObjectData")
-								&& jdata.getInt("Tag") == -10) {
-
-							endY = jdata.getJSONObject("Position").getInt("Y");
-
+							
+							float fov = (float) jdata.getDouble("Fov");
+							read.log("fov: "+fov);
+							dout.writeFloat(fov);
+							
+							JSONObject jdata1 = jdata.getJSONObject("ClipPlane");
+							
+							float ClipPlaneX = (float) jdata1.getDouble("X");
+							
+							read.log("ClipPlaneX: "+ClipPlaneX);
+							
+							dout.writeFloat(ClipPlaneX);
+							
+							float ClipPlaneY = (float) jdata1.getDouble("Y");
+							
+							read.log("ClipPlaneY: "+ClipPlaneY);
+							
+							dout.writeFloat(ClipPlaneY);
+							
+							
+							int CameraFlagMode = jdata.getInt("CameraFlagMode");
+							
+							read.log("CameraFlagMode: "+CameraFlagMode);
+							
+							dout.writeInt(CameraFlagMode);
+							
+							JSONObject jdata2 = jdata.getJSONObject("Position3D");
+							
+							float Position3DX =(float) jdata2.getDouble("X");
+							
+							read.log("Position3DX: "+Position3DX);
+							
+							dout.writeFloat(Position3DX);
+							
+							float Position3DY =(float) jdata2.getDouble("Y");
+							
+							read.log("Position3DY: "+Position3DY);
+							
+							dout.writeFloat(Position3DY);
+							
+							float Position3DZ =(float) jdata2.getDouble("Z");
+							
+							read.log("Position3DZ: "+Position3DZ);
+							
+							dout.writeFloat(Position3DZ);
+							
+							JSONObject jdata3 = jdata.getJSONObject("Rotation3D");
+							
+							float Rotation3DX = (float) jdata3.getDouble("X");
+							
+							read.log("Rotation3DX: "+Rotation3DX);
+							
+							dout.writeFloat(Rotation3DX);
+							
+							float Rotation3DY = (float) jdata3.getDouble("Y");
+							
+							read.log("Rotation3DY: "+Rotation3DY);
+							
+							dout.writeFloat(Rotation3DY);
+							
+							float Rotation3DZ = (float) jdata3.getDouble("Z");
+							
+							read.log("Rotation3DZ: "+Rotation3DZ);
+							
+							dout.writeFloat(Rotation3DZ);
+							
+							
+						}else if(jdata.getString("ctype").equals("Sprite3DObjectData")){
+							writeNum++;
+							
+							int type = 2;
+							read.log("type: " + type);
+							
+							dout.writeInt(type);
+							
+							String name = jdata.getString("Name");
+							read.log("name: "+name);
+							dout.writeUTF(name);
+							
+							String Path = jdata.getJSONObject("FileData").getString("Path");
+							
+							read.log("Path: "+Path);
+							dout.writeUTF(Path);
+							
+							int CameraFlagMode = jdata.getInt("CameraFlagMode");
+							
+							read.log("CameraFlagMode: "+CameraFlagMode);
+							
+							dout.writeInt(CameraFlagMode);
+							
+							int Tag = jdata.getInt("Tag");
+							
+							read.log("Tag: "+Tag);
+							
+							dout.writeInt(Tag);
+							
+							boolean isrun = jdata.getBoolean("RunAction3D");
+							
+							read.log("isrun: "+isrun);
+							
+							dout.writeBoolean(isrun);
+							
+							JSONObject jdata2 = jdata.getJSONObject("Position3D");
+							
+							float Position3DX =(float) jdata2.getDouble("X");
+							
+							read.log("Position3DX: "+Position3DX);
+							
+							dout.writeFloat(Position3DX);
+							
+							float Position3DY =(float) jdata2.getDouble("Y");
+							
+							read.log("Position3DY: "+Position3DY);
+							
+							dout.writeFloat(Position3DY);
+							
+							float Position3DZ =(float) jdata2.getDouble("Z");
+							
+							read.log("Position3DZ: "+Position3DZ);
+							
+							dout.writeFloat(Position3DZ);
+							
+							JSONObject jdata3 = jdata.getJSONObject("Rotation3D");
+							
+							float Rotation3DX = (float) jdata3.getDouble("X");
+							
+							read.log("Rotation3DX: "+Rotation3DX);
+							
+							dout.writeFloat(Rotation3DX);
+							
+							float Rotation3DY = (float) jdata3.getDouble("Y");
+							
+							read.log("Rotation3DY: "+Rotation3DY);
+							
+							dout.writeFloat(Rotation3DY);
+							
+							float Rotation3DZ = (float) jdata3.getDouble("Z");
+							
+							read.log("Rotation3DZ: "+Rotation3DZ);
+							
+							dout.writeFloat(Rotation3DZ);
+							
+							JSONObject jdata4 = jdata.getJSONObject("Scale3D");
+							
+							float Scale3DX = (float) jdata4.getDouble("X");
+							
+							read.log("Scale3DX: "+Scale3DX);
+							
+							dout.writeFloat(Scale3DX);
+							
+							float Scale3DY = (float) jdata4.getDouble("Y");
+							
+							read.log("Scale3DY: "+Scale3DY);
+							
+							dout.writeFloat(Scale3DY);
+							
+							float Scale3DZ = (float) jdata4.getDouble("Z");
+							
+							read.log("Scale3DZ: "+Scale3DZ);
+							
+							dout.writeFloat(Scale3DZ);
+							
+						}else if(jdata.getString("ctype").equals("Node3DObjectData")){
+							writeNum++;
+							
+							int type = 3;
+							read.log("type: " + type);
+							
+							dout.writeInt(type);
+							
+							String name = jdata.getString("Name");
+							read.log("name: "+name);
+							dout.writeUTF(name);
+							
+							int CameraFlagMode = jdata.getInt("CameraFlagMode");
+							
+							read.log("CameraFlagMode: "+CameraFlagMode);
+							
+							dout.writeInt(CameraFlagMode);
+							
+							int Tag = jdata.getInt("Tag");
+							
+							read.log("Tag: "+Tag);
+							
+							dout.writeInt(Tag);
+							
+							JSONObject jdata2 = jdata.getJSONObject("Position3D");
+							
+							float Position3DX =(float) jdata2.getDouble("X");
+							
+							read.log("Position3DX: "+Position3DX);
+							
+							dout.writeFloat(Position3DX);
+							
+							float Position3DY =(float) jdata2.getDouble("Y");
+							
+							read.log("Position3DY: "+Position3DY);
+							
+							dout.writeFloat(Position3DY);
+							
+							float Position3DZ =(float) jdata2.getDouble("Z");
+							
+							read.log("Position3DZ: "+Position3DZ);
+							
+							dout.writeFloat(Position3DZ);
+							
+							JSONObject jdata3 = jdata.getJSONObject("Rotation3D");
+							
+							float Rotation3DX = (float) jdata3.getDouble("X");
+							
+							read.log("Rotation3DX: "+Rotation3DX);
+							
+							dout.writeFloat(Rotation3DX);
+							
+							float Rotation3DY = (float) jdata3.getDouble("Y");
+							
+							read.log("Rotation3DY: "+Rotation3DY);
+							
+							dout.writeFloat(Rotation3DY);
+							
+							float Rotation3DZ = (float) jdata3.getDouble("Z");
+							
+							read.log("Rotation3DZ: "+Rotation3DZ);
+							
+							dout.writeFloat(Rotation3DZ);
+							
+							
+							JSONObject jdata4 = jdata.getJSONObject("Scale3D");
+							
+							float Scale3DX = (float) jdata4.getDouble("X");
+							
+							read.log("Scale3DX: "+Scale3DX);
+							
+							dout.writeFloat(Scale3DX);
+							
+							float Scale3DY = (float) jdata4.getDouble("Y");
+							
+							read.log("Scale3DY: "+Scale3DY);
+							
+							dout.writeFloat(Scale3DY);
+							
+							float Scale3DZ = (float) jdata4.getDouble("Z");
+							
+							read.log("Scale3DZ: "+Scale3DZ);
+							
+							dout.writeFloat(Scale3DZ);
 						}
 
 					}
-
-					read.log("endY: " + endY);
-
-					dout.writeInt(endY);
-
+					
+					
+					read.log("writeNum: "+writeNum);
+					
 					dout.flush();
 					bout.flush();
 
